ddeb8a597101f4bf6c8124b1309cfdb980fb9f63
Ramazan <ramazan.tsucharov@icloud.com>
2026-04-16 22:53:40 +0300

    Задание 7.15

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { offersReducer } from './reducer.ts';
+import {configureStore} from '@reduxjs/toolkit';
+import {offersReducer} from './reducer.ts';
 
 export const store = configureStore({
   reducer: {
