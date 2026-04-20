b3683a006d793e45f7d2b160fd322b32f5cec8b5
Ramazan <ramazan.tsucharov@icloud.com>
2026-04-20 21:56:19 +0300

    Задание 1 по 8 модулю

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -55,10 +55,14 @@ const MainScreen = (): React.ReactElement => {
     return <Spinner />;
   }
 
+  const isEmpty = currentCityOffers.length === 0;
+
   return (
     <>
       <Header />
-      <main className="page__main page__main--index">
+      <main
+        className={`page__main page__main--index${isEmpty ? ' page__main--index-empty' : ''}`}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -66,31 +70,47 @@ const MainScreen = (): React.ReactElement => {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">
-                {currentCityOffers.length} places to stay in {city}
-              </b>
-              <SortingList
-                currentSort={sortType}
-                onSortChange={handleSortChange}
-              />
-              <OffersList
-                offers={sortedOffers}
-                listClassName="cities__places-list places__list tabs__content"
-                onActiveOfferChange={setActiveOffer}
-              />
-            </section>
-            <div className="cities__right-section">
-              {currentCity && (
-                <Map
-                  city={currentCity}
-                  offers={currentCityOffers}
-                  selectedOffer={activeOffer}
-                />
-              )}
-            </div>
+          <div
+            className={`cities__places-container${isEmpty ? ' cities__places-container--empty' : ''} container`}
+          >
+            {isEmpty ? (
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">
+                    We could not find any property available at the moment in{' '}
+                    {city}
+                  </p>
+                </div>
+              </section>
+            ) : (
+              <>
+                <section className="cities__places places">
+                  <h2 className="visually-hidden">Places</h2>
+                  <b className="places__found">
+                    {currentCityOffers.length} places to stay in {city}
+                  </b>
+                  <SortingList
+                    currentSort={sortType}
+                    onSortChange={handleSortChange}
+                  />
+                  <OffersList
+                    offers={sortedOffers}
+                    listClassName="cities__places-list places__list tabs__content"
+                    onActiveOfferChange={setActiveOffer}
+                  />
+                </section>
+                <div className="cities__right-section">
+                  {currentCity && (
+                    <Map
+                      city={currentCity}
+                      offers={currentCityOffers}
+                      selectedOffer={activeOffer}
+                    />
+                  )}
+                </div>
+              </>
+            )}
           </div>
         </div>
       </main>
